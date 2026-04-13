ddbe3d86d27ab7b34a3634fcee91c706ce123b33
JoseGeraldo1 <firechip@proton.me>
2026-04-13 00:04:53 +0000

    Desafio: Nível Mestre: V1.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    // Definição das variáveis
     char estado1;
     char estado2;
     char codigo1[10];
@@ -23,7 +22,6 @@ int main() {
     float super_poder1;
     float super_poder2;
 
-    // Uso do 'printf' para introduzir o usuário ao programa
     printf("Olá, Jogador! Bem-vindo ao Super Trunfo Cidades!\n");
     printf("Vamos guiá-lo quanto às regras do jogo. É simples.\n");
     printf("Você fornecerá dados relacionados a duas Cidades.\n");
@@ -32,7 +30,6 @@ int main() {
 
     printf("=======================================================================================================================================================================\n\n");
     
-    // Uso do 'printf' em cojunto com 'scanf' e especificadores de formato para coleta de dados da Carta 1
     printf("Começaremos pela Carta 1:\n");
     printf("# Dicas: Ao inserir sua resposta, pressione a tecla enter para continuar!\n");
     printf("## Dicas: Caso precise apagar alguma coisa, é só pressionar a telca backspace!\n");
@@ -53,7 +50,6 @@ int main() {
 
     printf("\n=======================================================================================================================================================================\n\n");
 
-    // Uso do 'printf' e especificadores de formato para exibir a Carta 1
     printf("CARTA 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -63,11 +59,9 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
 
-    // Cálculo da densidade populacional para a Carta 1
     densidade1 = (float) populacao1 / area1;
     printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
 
-    // Cálculo do PIB per Capita para a Carta1
     pib_per1 = pib1 * 1000000000 / (float) populacao1;
     printf("PIB per Capita: %.2f reais\n", pib_per1);
 
@@ -78,7 +72,6 @@ int main() {
 
     printf("=======================================================================================================================================================================\n\n");
 
-    // Uso do 'printf' em cojunto com 'scanf' e especificadores de formato para coleta de dados da Carta 2
     printf("Agora, faremos o mesmo processo para a Carta 2:\n");
     printf("1) Estado: Você pode escolher uma letra de 'A' a 'H' para representá-lo (ex: B): ");
     scanf(" %c", &estado2);
@@ -97,7 +90,6 @@ int main() {
 
     printf("\n=======================================================================================================================================================================\n\n");
 
-    // Uso do 'printf' e especificadores de formato para exibir a Carta 2
     printf("CARTA 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
@@ -107,11 +99,9 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
-    // Cálculo da densidade populacional para a Carta 2
     densidade2 = (float) populacao2 / area2;
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
 
-    // Cálculo do PIB per Capita para a Carta 2
     pib_per2 = pib2 * 1000000000 / (float) populacao2;
     printf("PIB per Capita: %.2f reais\n", pib_per2);
 
